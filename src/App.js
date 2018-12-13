e92f2afb187d7e8f621535bbc331029c35ed341a
Lars Kristian Haga <LarsKHaga@gmail.com>
2018-12-13 20:13:09 +0100

    Added some text and reworked how the page is shown

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import oslo from './resources/BarcodeOslo.jpg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <img src={oslo}/>
-      </div>
-    );
-  }
+
+      render() {
+
+      let isRaining = false;
+
+        return (
+          <div className="App">
+
+            <img src={oslo} className="backgroundImage" alt="Background image of Oslo"/>
+
+            <div className="centerText">
+                {
+                isRaining
+                ?
+                <p>Ja</p>
+                :
+                <p>Faktisk ikke</p>
+                }
+            </div>
+          </div>
+        );
+      }
 }
 
 export default App;
