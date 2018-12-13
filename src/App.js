08c3bc7f4b820ce4f6da60b096db5766f83f6dc2
Lars Kristian Haga <LarsKHaga@gmail.com>
2018-12-13 21:50:06 +0100

    Finised adding rest call to weather service and displays the result to the webpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import oslo from './resources/BarcodeOslo.jpg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
 
-      render() {
+    render() {
 
-      let isRaining = false;
+        function isItRainingValue() {
+            let isRaining = null;
+
+            axios.get('http://dataservice.accuweather.com/currentconditions/v1/254946?apikey=SpNdTpUso1J9PKomtgcJSsD2XsGahrf5')
+                  .then(function (response) {
+                    isRaining = response.data[0].HasPrecipitation;
+                  })
+                  .catch(function (error) {
+                    isRaining = null;
+                  });
+            return isRaining;
+        };
 
         return (
           <div className="App">
@@ -14,13 +26,17 @@ class App extends Component {
             <img src={oslo} className="backgroundImage" alt="Background image of Oslo"/>
 
             <div className="centerText">
-                {
-                isRaining
-                ?
-                <p>Ja</p>
-                :
+            {
+            isItRainingValue() === null ?
+            <p>Error :
+            (
+            </p> :
+            (
+                isItRainingValue() ?
+                <p>Ja</p> :
                 <p>Faktisk ikke</p>
-                }
+             )
+            }
             </div>
           </div>
         );
