8f286926e170dc05470c33e314240405905a886a
Lars Kristian Haga <larsh@elkjop.no>
2022-09-30 16:49:50 +0200

    Added basic functionality for getting the API data, and showing a message for when its raining.

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,36 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <header>
-
-    <div class="wrapper">
-    </div>
-  </header>
-
-  <main>
-  </main>
+  <div class="app">
+    <pre>{{ isItRaining }}</pre>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import axios from "axios";
 
+export default {
+  name: "App",
+  data() {
+    return {
+      // Default value
+      isItRaining: "Dont know..."
+    };
+  },
+  mounted() {
+    const endpointURL = "https://www.yr.no/api/v0/locations/1-72837/forecast/now";
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    axios.get(endpointURL)
+      .then((response) => {
+        // Get the first element in the list, since this is the closest to now.
+        const isRainingValue = response.data.points[0].precipitation.intensity;
+
+        isRainingValue !== 0 ?
+          this.isItRaining = "Yes..."
+          :
+          this.isItRaining = "Nope!";
+      })
+      .catch((error) => {
+        console.log("Something went wrong. " + error);
+      });
   }
-  
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+};
+</script>
