2c6365c0b58db302ea6081157036f33a0f68794c
Lars Kristian Haga <larsh@elkjop.no>
2022-09-30 13:19:15 +0200

    Removed a bunch of unnecessary code.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
   </main>
 </template>
 
@@ -22,10 +17,6 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
 @media (min-width: 1024px) {
   header {
@@ -33,11 +24,7 @@ header {
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
+  
   header .wrapper {
     display: flex;
     place-items: flex-start;
