4368e814e9de3b91c737813b6a7419badca0b1fd
larskristianhaga <lars.kristian.haga@cappelendamm.no>
2023-06-21 20:27:38 +0200

    Revert "More logging."
    This reverts commit ba532d24fa03fc8fee766824ab4ac66e5810b241.

--- app/src/App.vue
+++ app/src/App.vue
@@ -9,29 +9,26 @@ export default {
   name: "App",
   data() {
     return {
-      isItRaining: "",
+      "isItRaining": ""
     };
   },
   mounted() {
-    // Endpoint is gotten from GitHub Actions Secret.
-    const endpointURL = import.meta.env.VITE_BACKEND_ENDPOINT_URL;
-    console.log(endpointURL);
+  // Endpoint is gotten from GitHub Actions Secret.
+  const endpointURL = import.meta.env.VITE_BACKEND_ENDPOINT_URL;
 
-    axios
-      .get(endpointURL)
+    axios.get(endpointURL)
       .then((response) => {
-        console.log(response);
-        console.log(response.data);
 
-        const isRainingValue = response.data["DoesItRain"];
+      const isRainingValue = response.data["DoesItRain"];
 
-        console.log(isRainingValue);
-
-        isRainingValue ? (this.isItRaining = "Ja") : (this.isItRaining = "Nei");
+        isRainingValue ?
+          this.isItRaining = "Ja..."
+          :
+          this.isItRaining = "Nei!";
       })
       .catch((error) => {
         console.log("Something went wrong. " + error);
       });
-  },
+  }
 };
 </script>
