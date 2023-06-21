ba532d24fa03fc8fee766824ab4ac66e5810b241
larskristianhaga <lars.kristian.haga@cappelendamm.no>
2023-06-21 16:22:51 +0200

    More logging.

--- app/src/App.vue
+++ app/src/App.vue
@@ -9,26 +9,29 @@ export default {
   name: "App",
   data() {
     return {
-      "isItRaining": ""
+      isItRaining: "",
     };
   },
   mounted() {
-  // Endpoint is gotten from GitHub Actions Secret.
-  const endpointURL = import.meta.env.VITE_BACKEND_ENDPOINT_URL;
+    // Endpoint is gotten from GitHub Actions Secret.
+    const endpointURL = import.meta.env.VITE_BACKEND_ENDPOINT_URL;
+    console.log(endpointURL);
 
-    axios.get(endpointURL)
+    axios
+      .get(endpointURL)
       .then((response) => {
+        console.log(response);
+        console.log(response.data);
 
-      const isRainingValue = response.data["DoesItRain"];
+        const isRainingValue = response.data["DoesItRain"];
 
-        isRainingValue ?
-          this.isItRaining = "Ja..."
-          :
-          this.isItRaining = "Nei!";
+        console.log(isRainingValue);
+
+        isRainingValue ? (this.isItRaining = "Ja") : (this.isItRaining = "Nei");
       })
       .catch((error) => {
         console.log("Something went wrong. " + error);
       });
-  }
+  },
 };
 </script>
