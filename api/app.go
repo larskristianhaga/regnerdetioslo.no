8efaa32d89e2b0fcffa4feef163eadfbf83be6c7
Lars Kristian Haga <larskhaga@gmail.com>
2024-12-12 22:26:26 +0100

    Changes to the code to move from function to API.

--- api/app.go
+++ api/app.go
@@ -1,59 +1,69 @@
-package regnerdetioslo
+package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"io"
 	"log"
 	"net/http"
-
-	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"os"
 )
 
-func init() {
-	functions.HTTP("RegnerDetIOsloFunction", RegnerDetIOsloFunction)
-}
+var endpoint = "https://www.yr.no/api/v0/locations/1-72837/forecast/currenthour"
 
-func RegnerDetIOsloFunction(w http.ResponseWriter, r *http.Request) {
-
-	// API endpoint.
-	endpoint :=
-		"https://www.yr.no/api/v0/locations/1-72837/forecast/currenthour"
-
-	// Get data from the API.
-	response, err := http.Get(endpoint)
-
-	// Guard for empty response.
-	if err != nil {
-		log.Fatal(err.Error())
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
 
-	// Get the JSON data out of the response.
-	responseData, err := io.ReadAll(response.Body)
+	http.HandleFunc("/check", func(w http.ResponseWriter, r *http.Request) {
+		// Create a custom HTTP client with disabled SSL verification.
+		customTransport := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client := &http.Client{Transport: customTransport}
 
-	// Guard for something wrong with reading the content.
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+		// Get data from the API using the custom client.
+		response, err := client.Get(endpoint)
 
-	var yr Yr
-	// JSONIFY the string.
-	_ = json.Unmarshal(responseData, &yr)
+		// Guard for empty response.
+		if err != nil {
+			log.Fatal(err.Error())
+		}
 
-	// Extract the values I want.
-	doesItRain := yr.Precipitation.Value > 0
-	lastUpdatedAt := yr.Created
+		// Get the JSON data out of the response.
+		responseData, err := io.ReadAll(response.Body)
 
-	// Enable CORS.
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+		// Guard for something wrong with reading the content.
+		if err != nil {
+			log.Fatal(err.Error())
+		}
 
-	// Set the content type to JSON.
-	w.Header().Set("Content-Type", "application/json")
+		var yr Yr
+		// JSONIFY the string.
+		_ = json.Unmarshal(responseData, &yr)
 
-	// Return the object.
-	_ = json.NewEncoder(w).Encode(struct {
-		DoesItRain   bool
-		DataFromTime string
-	}{DoesItRain: doesItRain, DataFromTime: lastUpdatedAt})
+		// Extract the values I want.
+		doesItRain := yr.Precipitation.Value > 0
+		lastUpdatedAt := yr.Created
+
+		// Enable CORS.
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		// Set the content type to JSON.
+		w.Header().Set("Content-Type", "application/json")
+
+		// Return the object.
+		_ = json.NewEncoder(w).Encode(struct {
+			DoesItRain   bool
+			DataFromTime string
+		}{DoesItRain: doesItRain, DataFromTime: lastUpdatedAt})
+
+	})
+
+	log.Println("App live and listening on port: ", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 type Yr struct {
